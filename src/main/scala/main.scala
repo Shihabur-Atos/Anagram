b118fa79bc1d2acd2291bbcae7eedb09f2bff342
Shihabur-Atos <98172596+Shihabur-Atos@users.noreply.github.com>
2022-10-07 14:32:50 +0100

    Working import of word dictionary as a list

--- src/main/scala/main.scala
+++ src/main/scala/main.scala
@@ -1,20 +1,25 @@
-import scala.:+
+import scala.Console.println
 import scala.io.Source
 
 object main extends App {
-  var word: String = "documenting".toList.sorted.toString()
-  println(word)
+  var keyword: List[Char] = "documenting".toList
+
+  println(keyword)
 
   val source = Source.fromFile("word_list.txt")
-  var list: List[String] = List()
-  for (line <- source.getLines()) {
-    println(line)
-    if(line.toList.sorted.toString.equals(word)) {
-      println("hello")
-      list = list :+ line
-    }
+  var list: List[String] = source.getLines().toList.map(_.split("\\s+").mkString("",",",""))
+  var splitList : List[String] = List()
+  for(line <- list) {
+    val words = line.trim.split(",").toList
+    println(words + " size: " + words.size.toString)
+    splitList = splitList ::: words
   }
   source.close()
+  for(word <- splitList) {
+    if(!(word.length > keyword.size)) {
+      word
+    }
 
-
+  }
 }
+
