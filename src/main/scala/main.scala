13d4613e524d91401832bdf4c5059ed254067043
Shihabur-Atos <98172596+Shihabur-Atos@users.noreply.github.com>
2022-10-07 16:15:11 +0100

    Working method to find all possible permutations of words from the keyword

--- src/main/scala/main.scala
+++ src/main/scala/main.scala
@@ -1,25 +1,39 @@
 import scala.Console.println
-import scala.io.Source
+import scala.io.{BufferedSource, Source}
 
 object main extends App {
-  var keyword: List[Char] = "documenting".toList
-
+  val keyword : String = "documenting"
   println(keyword)
 
-  val source = Source.fromFile("word_list.txt")
+  val source: BufferedSource = Source.fromFile("word_list.txt")
   var list: List[String] = source.getLines().toList.map(_.split("\\s+").mkString("",",",""))
   var splitList : List[String] = List()
   for(line <- list) {
-    val words = line.trim.split(",").toList
-    println(words + " size: " + words.size.toString)
+    val words: List[String] = line.trim.split(",").toList
     splitList = splitList ::: words
   }
+  var cleanedList : List[String] = List();
+  cleanedList = splitList.filterNot(findWords(keyword,splitList).toSet)
+  println(cleanedList)
   source.close()
-  for(word <- splitList) {
-    if(!(word.length > keyword.size)) {
-      word
-    }
 
+  def findWords(mainWord : String, dict : List[String]) : List[String] = {
+    var notValidWords : List[String] = List()
+    for (word <- dict) {
+      if (!(word.length > mainWord.length)) {
+        // make keyword into a set and filtering each word to see if there are letters that arent in documenting
+        println("Current word: " + word + ", " + "Current keyword: " + mainWord)
+        val comparedDiff = word.filterNot(mainWord.toSet)
+        if (comparedDiff.isEmpty) {
+          println("Anagram: " + word)
+        } else {
+          val tempList = List(word)
+          notValidWords = notValidWords ::: tempList
+          println("Not valid word due to " + comparedDiff + " not being in the keyword")
+        }
+      }
+    }
+    notValidWords
   }
 }
 
