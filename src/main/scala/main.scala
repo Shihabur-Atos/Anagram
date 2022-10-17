f8bda551ff8f063a93661a6ca0e1f60481ab5e71
Shihabur-Atos <98172596+Shihabur-Atos@users.noreply.github.com>
2022-10-17 10:30:50 +0100

    Simplified code and fixed issues - need to create tests.

--- src/main/scala/main.scala
+++ src/main/scala/main.scala
@@ -1,4 +1,5 @@
 import scala.Console.println
+import scala.annotation.tailrec
 import scala.io.{BufferedSource, Source}
 
 object main extends App {
@@ -6,30 +7,30 @@ object main extends App {
   val keyword: String = "documenting"
   val wordListFilePath: String = "word_list.txt"
   val wordsFromFile = readWordsFromFile(wordListFilePath)
-  val anagramsFromFile = getAnagramsFromWordList(keyword,wordsFromFile)
-  println(anagramsFromFile)
-  findValidAnagramPairs(keyword,anagramsFromFile)
-  def readWordsFromFile(filePath: String) : Seq[String] = {
+  val anagramsFromFile = getAnagramsFromWordList(keyword, wordsFromFile)
+  findValidAnagramPairs(keyword, anagramsFromFile)
+
+  def readWordsFromFile(filePath: String): Seq[String] = {
     val filePathSource: BufferedSource = Source.fromFile(filePath)
-    val getFormattedLinesFromFile: Seq[String] = filePathSource.getLines().map(_.split("\\s+").mkString("",",","")).toSeq
+    val getFormattedLinesFromFile: Seq[String] = filePathSource.getLines().map(_.split("\\s+").mkString("", ",", "")).toSeq
     getFormattedLinesFromFile.flatMap(line => line.trim.split(","))
   }
 
   def getAnagramsFromWordList(keyWord: String, wordsFromFile: Seq[String]): Seq[String] = {
     val word = for {
       word <- wordsFromFile
-      if (word.intersect(keyWord).equals(word))
+      if word.intersect(keyWord).equals(word)
     } yield word
-    word.sortWith((s,t) => s.length < t.length)
+    word.sortWith((s, t) => s.length < t.length)
   }
 
+  @tailrec
   def findValidAnagramPairs(keyWord: String, anagramsFromFile: Seq[String]): Unit = {
     val filteredKeyWord: String = keyWord.filterNot(anagramsFromFile.head.toSet)
     if (!(anagramsFromFile.size < 2)) {
       for (word <- anagramsFromFile) {
         if (word.intersect(filteredKeyWord).equals(word)) {
-          println("Pair found: " + anagramsFromFile.head, word)
-          findValidAnagramPairs(keyWord, anagramsFromFile.filterNot(Seq(anagramsFromFile.head,word).toSet))
+          println(s"Pair found: ${anagramsFromFile.head}, $word")
         }
       }
       findValidAnagramPairs(keyWord, anagramsFromFile.filterNot(Seq(anagramsFromFile.head).toSet))
